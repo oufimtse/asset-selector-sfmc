24cc2966a7f117d2621e90e454ed1c0f09a648cc
Oleksandr Ufimtsev <oufimtse@adobe.com>
2025-10-19 15:12:50 -0400

    update

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 const imsProps = {
     imsClientId: 'aemcs-sasha-assetselector',
-    imsOrg: '76B329395DF155D60A495E2C@AdobeOrg/',
+    imsOrg: '76B329395DF155D60A495E2C@AdobeOrg',
     imsScope: 'AdobeID,openid,additional_info.projectedProductContext,read_organizations',
     redirectUrl: window.location.href,
     modalMode: true,
