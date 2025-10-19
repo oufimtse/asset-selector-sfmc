4af89a8495140eb2c0baab58a3418dd3602583ae
Oleksandr Ufimtsev <oufimtse@adobe.com>
2025-10-19 14:51:30 -0400

    update

---
+++ common.js
@@ -0,0 +1,19 @@
+const imsProps = {
+    imsClientId: 'aemcs-sasha-assetselector',
+    imsOrg: '76B329395DF155D60A495E2C@AdobeOrg/',
+    imsScope: 'AdobeID,openid,additional_info.projectedProductContext,read_organizations',
+    redirectUrl: window.location.href,
+    modalMode: true,
+    onImsServiceInitialized: (service) => {
+        console.log('onImsServiceInitialized', service);
+    },
+    onAccessTokenReceived: (token) => {
+        console.log('onAccessTokenReceived', token);
+    },
+    onAccessTokenExpired: () => {
+        console.log('onAccessTokenError');
+    },
+    onErrorReceived: (type, msg) => {
+        console.log('onErrorReceived', type, msg);
+    },
+};
